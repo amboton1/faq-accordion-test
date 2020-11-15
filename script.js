dea340906ce0ec4da5663d3af0919b81d36e1906
ammar <ammar.botonjic@convergent-usa.com>
2020-11-15 13:00:28 +0100

    Changes

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function renderQuestionsAnswer() {
     for (let index = 0; index < question.length; index++) {
         question[index].addEventListener('click', () => {
             let answerContent = question[index].lastElementChild.lastElementChild;
-            let faqArrow = question[index].firstElementChild.firstElementChild;
-    
-            faqArrow.classList.toggle('fa-chevron-up')
-            answerContent.classList.toggle('show-answer')
+            let boldTitle = question[index].firstElementChild.firstElementChild;
+            let faqArrow = question[index].firstElementChild.firstElementChild.nextElementSibling;
+            console.log(faqArrow);
+            
+            boldTitle.classList.toggle('bold');
+            faqArrow.classList.toggle('fa-chevron-up');
+            answerContent.classList.toggle('show-answer');
         })
     }
 }
